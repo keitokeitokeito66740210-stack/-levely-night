3ee257592c7ebc2e7e7db801f868856d87d71cd3
keitokeitokeito66740210-stack <keito.keito.keito.66740210@gmail.com>
2026-08-28 11:55:32 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.LEVELY_CONFIG = {
-  SUPABASE_URL: "https://YOUR_PROJECT.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_KEY"
+  SUPABASE_URL: "https://aisjildhxxrvihhxfhrn.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_7t5q2K34tC2-B1IGZNFU1w_0ozzkiYi"
 };
